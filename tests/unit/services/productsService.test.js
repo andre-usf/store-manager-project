8b9c726de2024829524d2e86e76724529bbde2da
andre-usf <andrefretta@gmail.com>
2023-02-13 18:13:35 -0300

    test: cria testes unitários da camada service referente às rotas dos produtos.

---
+++ tests/unit/services/productsService.test.js
@@ -0,0 +1,39 @@
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const productsModel = require('../../../src/models/productsModel');
+const productsService = require('../../../src/services/productsService');
+const products = require('./mocks/productsService.mock');
+
+describe('Testes unitários da camada service referente às rotas dos produtos', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+
+  describe('Quando acessada a rota "/products"', function () {
+    it('Deve trazer todos os produtos por meio da função "getAll"', async function () {
+      sinon.stub(productsModel, 'getAll').resolves(products);
+
+      const result = await productsService.getAll();
+
+      expect(result).to.be.deep.equal(products);
+    });
+  });
+
+  describe('Quando acessada a rota "/products/:id" ', function () {
+    it('Deve trazer o produto do id correspondente se o produto existir', async function () {
+      sinon.stub(productsModel, 'findById').resolves([products[1]]);
+
+      const result = await productsService.findById(2);
+
+      expect(result).to.be.deep.equal(products[1]);
+    });
+    it('Deve trazer a mensagem "message": "Product not found" se o produto não existir', async function () {
+      sinon.stub(productsModel, 'findById').resolves(undefined);
+
+      const result = await productsService.findById(3);
+
+      expect(result.message).to.be.equal("Product not found");
+    });
+  });
+});
