fbb7bf67c6dd8979ead64883081b6e6198b1a96c
andre-usf <andrefretta@gmail.com>
2023-02-14 10:09:06 -0300

    refactor: errorMessage.

--- src/services/validation/validationProductName.js
+++ src/services/validation/validationProductName.js
@@ -2,12 +2,14 @@ const schemas = require('./schemas');
 
 const typeErrorReplace = (error) => error.details[0].type.toUpperCase().replace('.', '_');
 
+const errorMessage = (error) => error.details[0].message;
+
 const validateProductName = (productName) => {
   const { error } = schemas.productNameSchema.validate(productName);
   if (error) {
     return {
       type: typeErrorReplace(error),
-      result: { message: error.details[0].message },
+      result: { message: errorMessage(error) },
     };
   }
   return { type: null, result: '' };
