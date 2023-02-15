faa28655a5a75a8b6f0ae47c56f06eb76893a794
andre-usf <andrefretta@gmail.com>
2023-02-15 14:21:58 -0300

    test: cria testes unitários para vendas inválidas  na camada service do endpoint POST /sales.

--- tests/unit/services/mocks/salesService.mock.js
+++ tests/unit/services/mocks/salesService.mock.js
@@ -9,9 +9,55 @@ const validSales = [
   }
 ];
 
+const saleWithoutProductId = [
+  {
+    productId: 1,
+    quantity: 1
+  },
+  {
+    quantity: 5
+  }
+];
+
+const saleWithoutQuantity = [
+  {
+    productId: 1,
+    quantity: 1
+  },
+  {
+    productId: 2
+  }
+];
+
+const saleZeroQuantity = [
+  {
+    productId: 1,
+    quantity: 1
+  },
+  {
+    productId: 1,
+    quantity: 0
+  }
+];
+
+const nonexistentProductId = [
+  {
+    productId: 1,
+    quantity: 1
+  },
+  {
+    productId: 11,
+    quantity: 1
+  }
+];
+
 const findByIdResult = { type: null, result: '' };
 
 module.exports = {
   validSales,
+  saleWithoutProductId,
+  saleWithoutQuantity,
+  saleZeroQuantity,
+  nonexistentProductId,
   findByIdResult,
 };
--- tests/unit/services/salesService.test.js
+++ tests/unit/services/salesService.test.js
@@ -22,5 +22,34 @@ describe('Testes unitários da camada service referente às rotas das vendas - P
       expect(result.type).to.be.equal(null);
       expect(result.result).to.be.deep.equal(1);
     });
+    it('Deve trazer a mensagem ""productId" is required" se na venda não constar productId', async function () {
+
+      const result = await salesService.createSales(salesMock.saleWithoutProductId);
+
+      expect(result.type).to.be.equal('ANY_REQUIRED')
+      expect(result.result.message).to.be.equal('"productId" is required');
+    });
+    it('Deve trazer a mensagem ""quantity" is required" se na venda não constar quantity', async function () {
+
+      const result = await salesService.createSales(salesMock.saleWithoutQuantity);
+
+      expect(result.type).to.be.equal('ANY_REQUIRED')
+      expect(result.result.message).to.be.equal('"quantity" is required');
+    });
+    it('Deve trazer a mensagem ""quantity" must be greater than or equal to 1" se na venda constar quantity igual ou menor que zero', async function () {
+  
+      const result = await salesService.createSales(salesMock.saleZeroQuantity);
+
+      expect(result.type).to.be.equal('NUMBER_MIN')
+      expect(result.result.message).to.be.equal('"quantity" must be greater than or equal to 1');
+    });
+    it('Deve trazer a mensagem "Product not found" se na venda constar um productId inexistente no banco de dados', async function () {
+      sinon.stub(productsService, 'findById').resolves({ type: 'PRODUCT_NOT_FOUND', result: { message: 'Product not found' } });
+
+      const result = await salesService.createSales(salesMock.nonexistentProductId);
+      
+      expect(result.type).to.be.equal('PRODUCT_NOT_FOUND')
+      expect(result.result.message).to.be.equal('Product not found');
+    });
   });
 });
