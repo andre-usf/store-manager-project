7db695437e675bd7da12d1e6d3fc3259059d5949
andre-usf <andrefretta@gmail.com>
2023-03-08 00:24:04 -0300

    test: cria testes unitários para productsModel.

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -5,13 +5,13 @@ const connection = require('../../../src/models/connection');
 const productsModel = require('../../../src/models/productsModel');
 const products = require('./mocks/productsModel.mock');
 
-describe('Testes unitários da camada model referente às rotas dos produtos', function () {
+describe('Testes unitários da camada model referente aos produtos', function () {
   afterEach(function () {
     sinon.restore();
   });
   
-  describe('Quando acessada a rota GET "/products"', function () {
-    it('Deve trazer todos os produtos por meio da função "getAll"', async function () {
+  describe('Quando chamada a função "getAll"', function () {
+    it('Deve trazer todos os produtos', async function () {
       sinon.stub(connection, 'execute').resolves([products]);
 
       const result = await productsModel.getAll();
@@ -20,8 +20,8 @@ describe('Testes unitários da camada model referente às rotas dos produtos', f
     });
   });
 
-  describe('Quando acessada a rota GET "/products/:id"', function () {
-    it('Deve trazer o produto do id correspondente por meio da função "findById"', async function () {
+  describe('Quando chamada a função "findById"', function () {
+    it('Deve trazer o produto do id correspondente', async function () {
       sinon.stub(connection, 'execute').resolves([[products[1]]]);
 
       const result = await productsModel.findById(2);
@@ -30,7 +30,7 @@ describe('Testes unitários da camada model referente às rotas dos produtos', f
     });
   });
 
-  describe('Quando acessada a rota POST "/products"', function () {
+  describe('Quando chamada a função "insert"', function () {
     it('Deve inserir o produto e retornar um objeto com o id do produto cadastrado', async function () {
       const insertId = 3;
 
@@ -46,7 +46,7 @@ describe('Testes unitários da camada model referente às rotas dos produtos', f
     });
   });
 
-  describe('Quando acessada a rota PUT "/products/:id"', function () {
+  describe('Quando chamada a função "updateProduct"', function () {
     it('Deve alterar o produto referente ao id passado por parâmetro caso o nome seja válido e o produto exista', async function () {
       const changedRows = 1;
       
@@ -62,7 +62,7 @@ describe('Testes unitários da camada model referente às rotas dos produtos', f
     });
   });
 
-  describe('Quando acessada a rota DELETE "/products/:id"', function () {
+  describe('Quando chamada a função "deleteProduct"', function () {
     it('Deve deletar o produto referente ao id passado por parâmetro caso o produto exista', async function () {
       const affectedRows = 1;
 
@@ -73,6 +73,18 @@ describe('Testes unitários da camada model referente às rotas dos produtos', f
       const result = await productsModel.deleteProduct(productId);
 
       expect(result).to.be.equal(affectedRows);
+    });
+  });
+
+  describe('Quando chamada a função "searchProductByQuery"', function () {
+    it('Deve retornar um array com os produtos que foram encontrados pela busca', async function () {
+      const query = 'Martelo';
+      
+      sinon.stub(connection, 'query').resolves([[products[0]]]);
+
+      const result = await productsModel.searchProductByQuery(query);
+
+      expect(result).to.be.deep.equal([products[0]]);
     });
   });
 });
